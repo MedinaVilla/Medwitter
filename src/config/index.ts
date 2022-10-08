b743a6ddccc4b6922b7f31d0fab2a3286b382976
MedinaVilla <net_medina@hotmail.com>
2022-10-07 20:07:37 -0500

    Front prod config

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,17 +2,8 @@
 import {environment} from "src/environments/environment";
 
 console.log(environment)
-const isLocalhost = Boolean(
-    window.location.hostname === "localhost" ||
-    // [::1] is the IPv6 localhost address.
-    window.location.hostname === "[::1]" ||
-    // 127.0.0.1/8 is considered localhost for IPv4.
-    window.location.hostname.match(
-        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
-);
 
-const SERVER_URL = isLocalhost
+const SERVER_URL = !environment.production
     ? "http://localhost:3000"
     :  environment;
 
