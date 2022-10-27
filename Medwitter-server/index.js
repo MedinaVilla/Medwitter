b3293c949594d1ebcf53c14ea9a7e04420826c8b
MedinaVilla <net_medina@hotmail.com>
2022-10-27 17:18:15 -0500

    Medwitter v1 ready! Sombe bugs fixes

--- Medwitter-server/index.js
+++ Medwitter-server/index.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "dev")
   allowOrigin = "*"
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', allowOrigin);
+  res.setHeader('Access-Control-Allow-Origin',"*");
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
