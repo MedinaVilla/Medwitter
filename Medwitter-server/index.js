3a3a0a633ab9af87b04fabe6ab8f778c276980c0
MedinaVilla <net_medina@hotmail.com>
2022-10-29 17:57:20 -0500

    Prueba SSE on prod

--- Medwitter-server/index.js
+++ Medwitter-server/index.js
@@ -20,7 +20,6 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin',"*");
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', '*');
-  res.setHeader('Cache-control', 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0')
   // res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
--- Medwitter-server/routes/sse.js
+++ Medwitter-server/routes/sse.js
@@ -2,11 +2,27 @@ const express = require("express");
 const sse = require("../sse");
 
 const router = express.Router();
+const EventEmitter = require('events').EventEmitter;
 
-router.get("/stream", (req, res, next) => {
-    res.flush = () => { };
-    // res.flush();
+router.get("/stream", async(req, res, next) => {
+    res.set({
+        'Cache-Control': 'no-cache',
+        'Content-Type': 'text/event-stream',
+        'Connection': 'keep-alive'
+      });
+      res.flushHeaders();
+      res.write('retry: 10000\n\n');
+      let count = 0;
+  
+      while (true) {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+  
+        console.log('Emit', ++count);
+        // Emit an SSE that contains the current 'count' as a string
+        res.write(`data: ${count}\n\n`);
+      }
+
     next();
-}, sse.init);
+});
 
 module.exports = router;
