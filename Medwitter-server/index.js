2c610b3936606b2b583b69d79a0d25075a713b77
MedinaVilla <net_medina@hotmail.com>
2022-10-29 17:20:10 -0500

    Prueba SSE on prod

--- Medwitter-server/index.js
+++ Medwitter-server/index.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === "dev")
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin',"*");
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Headers', '*');
   // res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
