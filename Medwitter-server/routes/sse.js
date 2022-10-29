a2bfbc042081bde407caa287731bdd0369a81ff2
MedinaVilla <net_medina@hotmail.com>
2022-10-29 18:10:41 -0500

    Prueba SSE on prod

--- Medwitter-server/routes/sse.js
+++ Medwitter-server/routes/sse.js
@@ -4,24 +4,27 @@ const sse = require("../sse");
 const router = express.Router();
 // const EventEmitter = require('events').EventEmitter;
 
-router.get("/stream", async(req, res, next) => {
-    res.write("xD")
+router.get("/stream", async (req, res, next) => {
+    console.log("1")
     res.set({
         'Cache-Control': 'no-cache',
         'Content-Type': 'text/event-stream',
         'Connection': 'keep-alive'
-      });
-      res.flushHeaders();
-      res.write('retry: 10000\n\n');
-      let count = 0;
-  
-      while (true) {
+    });
+    console.log("2")
+    res.flushHeaders();
+    console.log("3")
+
+    res.write('retry: 10000\n\n');
+    let count = 0;
+
+    while (true) {
         await new Promise(resolve => setTimeout(resolve, 1000));
-  
+
         console.log('Emit', ++count);
         // Emit an SSE that contains the current 'count' as a string
         res.write(`data: ${count}\n\n`);
-      }
+    }
 
     next();
 });
